cc24291199956888a689516c3d579751852a0d4f
StarshineC <jonathanresponilla@gmail.com>
2024-08-14 12:46:53 -0400

    I think it's done actually

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 
-
-
 def main():
     inDir  = sys.argv[1]
     outDir = sys.argv[2]
 
     # Get file list
+    files = os.listdir(inDir)
 
-    # For each file:
-    for file in os.listdir(inDir):
+    # No Files
+    if len(files) == 0 :
+        stdio.writeln("-=-=-= No files to Sort! =-=-=-")
+        return
+    
+    # Yes Files
+    for file in files:
         # Get Year
-
+        path = f'{inDir}/{file}'
+        createdTime = os.path.getctime(path)
+        createdYear = int(time.ctime(createdTime)[-4:])
         # Move to a Folder
-        stdio.writeln(f'{inDir}')
+        if not os.path.exists(f'{outDir}/{createdYear}'): os.makedirs(f'{outDir}/{createdYear}')
+        os.rename(path, f'{outDir}/{createdYear}/{file}')
+        stdio.writeln(f'Moved {path} to {outDir}/{createdYear}/{file}')
+
+
+    stdio.writeln("-=-=-= Completed Sorting! =-=-=-")
+    return
 
 
 if __name__ == "__main__":
