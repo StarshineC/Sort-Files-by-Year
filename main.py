7f17b2125e506acd7bd2818c5ddc094ec63f7aef
StarshineC <jonathanresponilla@gmail.com>
2024-08-14 15:27:42 -0400

    Fixed an issue where directories won't sont into the "other" folder. Enhanced console log messages.

--- main.py
+++ main.py
@@ -12,39 +12,38 @@ def main():
 
         # No Directory?
         if not os.path.exists(dir):
-            stdio.writeln(f'"{dir}" does not exist...')
+            stdio.writeln(f'| "{dir}" does not exist...')
             continue
-        
-        # Sorting Message
-        stdio.writeln(f'Sorting "{dir}"...')
         
         # List of files in directory
         files = os.listdir(dir)
 
         # No Files?
         if len(files) == 0:
-            stdio.writeln(f'No files in "{dir}"')
+            stdio.writeln(f'| No files in "{dir}"')
             continue
-        
+
         # Yes Files?
+        stdio.writeln(f'| Sorting "{dir}"...')
         for file in files:
             path = f'{dir}/{file}'
             
             # Is Directory?
-            if not os.path.isfile(file):
+            if not os.path.isfile(path):
                 if not os.path.exists(f'{outDir}/other'): os.makedirs(f'{outDir}/other')
                 os.rename(path, f'{outDir}/other/{file}')
+                stdio.writeln(f'|| Moved {file} (other)')
             # Other Files
             else:
                 # Get Year
                 createdTime = os.path.getctime(path)
                 createdYear = int(time.ctime(createdTime)[-4:])
                 # Move to a Folder
-                print(f'{outDir}/{createdYear}')
                 if not os.path.exists(f'{outDir}/{createdYear}'): os.makedirs(f'{outDir}/{createdYear}')
                 os.rename(path, f'{outDir}/{createdYear}/{file}')
-                stdio.writeln(f'Moved {file}')
+                stdio.writeln(f'|| Moved {file} ({createdYear})')
     
+    stdio.writeln("Done!")
     return
 
     """
