57ea75aabad4755183e1ab57f75c2a3df24d9a7f
StarshineC <jonathanresponilla@gmail.com>
2024-08-14 11:50:35 -0400

    Initial File upload. Figure out how .gitignore works.

---
+++ main.py
@@ -0,0 +1,24 @@
+
+
+
+def main():
+    inDir  = sys.argv[1]
+    outDir = sys.argv[2]
+
+    # Get file list
+
+    # For each file:
+    for file in os.listdir(inDir):
+        # Get Year
+
+        # Move to a Folder
+        stdio.writeln(f'{inDir}')
+
+
+if __name__ == "__main__":
+    import sys
+    import stdio
+    import os
+    import time
+    
+    main();
